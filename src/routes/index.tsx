6158969aaa91348a90b170fa8b8bffdac7ea9627
Rodrigo Peralta <49768802+rodrigoperaltadev@users.noreply.github.com>
2021-11-26 19:31:26 -0300

    Add routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Home, Detail } from '../views'
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="detail/:id" element={<Detail />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes
