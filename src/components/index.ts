8aea403af641cc98cc06a8902507e1cb4fa62efe
Rodrigo Peralta <49768802+rodrigoperaltadev@users.noreply.github.com>
2021-11-26 19:32:20 -0300

    add index file on components

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Navbar } from "./Navbar";
+export { default as CountryCard } from "./CountryCard";
+export { default as CountriesList } from "./CountriesList";
+export { default as FilterCountries } from "./FiltersCountries";
+export { default as ContinentFilter } from "./ContinentFilter";
