a9321a4128d1ad73a2430935b5f796550b2ececa
Rodrigo Peralta <49768802+rodrigoperaltadev@users.noreply.github.com>
2021-11-26 19:26:42 -0300

    Add Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <header className='navbar'>
+      <div className='navbar__title'>COUNTRIES APP</div>
+    </header>
+  )
+}
+
+export default Navbar
